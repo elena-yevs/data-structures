62b6b88898ea79dde0ae5bd4e700ec230c4e0a51
Elena Yevsyukova <elena.yevs@gmail.com>
2020-12-05 22:42:00 +0200

    adding iface ShopQueue with methods; pressing alt+ctrl+L for code formatting

---
+++ src/main/java/com/example/ShopQueue.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public interface ShopQueue {
+    void amount();
+
+    void head();
+
+    void tail();
+
+    void serve();
+
+    void free();
+
+    void add();
+
+    void cut();
+
+    void skip();
+
+    void stop();
+}
