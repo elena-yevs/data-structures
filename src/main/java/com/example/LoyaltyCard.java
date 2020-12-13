1139bb3458a48370693a391ebd18168bf45984d1
Elena Yevsyukova <elena.yevs@gmail.com>
2020-12-13 12:22:36 +0200

    Javadoc for LoyaltyCard class

--- src/main/java/com/example/LoyaltyCard.java
+++ src/main/java/com/example/LoyaltyCard.java
@@ -2,17 +2,29 @@ package com.example;
 import com.google.common.base.Preconditions;
 
 
-
+/**
+ * Class implements Value Object
+ */
 public class LoyaltyCard {
+    /**
+     * Customer's card id
+     */
     private final String id;
 
+    /**
+     *Constructor
+     * @param id is verified by checkNotNull
+     */
      public LoyaltyCard(String id);
 
     {
         this.id = checkNotNull(id);
     }
 
-    public String value() {
+    /**
+     * Method returns customer's card id
+     */
+    public String id() {
         return id;
     }
 }
