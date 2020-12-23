20328610ee850b3f5dbe7d463d16ba353802760a
Elena Yevsyukova <elena.yevs@gmail.com>
2020-12-23 21:31:13 +0200

    Changes in formatting Javadoc for Customer.java

--- src/main/java/com/example/Customer.java
+++ src/main/java/com/example/Customer.java
@@ -2,6 +2,7 @@ package com.example;
 
 /**
  * A customer is a person who visits a shop for buying goods.
+ *
  * The customer can have a loyaltyCard that has a specific number.
  */
 public final class Customer {
@@ -9,7 +10,7 @@ public final class Customer {
     private final String loyaltyCard;
 
     /**
-     * Constructor for determination the customer's loyaltyCard number.
+     * Constructor for determination the customer's loyalty сard number.
      *
      * @param loyaltyCard String value
      */
