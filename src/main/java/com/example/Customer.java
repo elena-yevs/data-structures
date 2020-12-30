3b579596fda6da2b4d1352a59555f20063630a45
Elena Yevsyukova <elena.yevs@gmail.com>
2020-12-30 12:30:24 +0200

    Renaming the field `LoyaltyCard` to `card`; Javadoc for constructor is deleted.

--- src/main/java/com/example/Customer.java
+++ src/main/java/com/example/Customer.java
@@ -2,19 +2,13 @@ package com.example;
 
 /**
  * A customer is a person who visits a shop for buying goods.
- *
- * The customer can have a loyaltyCard that has a specific number.
+ * <p>
+ * The customer can have a loyalty card that has a specific number.
  */
 public final class Customer {
+    private final LoyaltyCard card;
 
-    private final LoyaltyCard loyaltyCard;
-
-    /**
-     * Constructor for determination the customer's loyalty card number.
-     *
-     * @param loyaltyCard
-     */
-    public Customer(LoyaltyCard loyaltyCard) {
-        this.loyaltyCard = loyaltyCard;
+    public Customer(LoyaltyCard card) {
+        this.card = card;
     }
 }
