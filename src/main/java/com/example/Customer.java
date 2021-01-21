10165f8dd971e0622da8935ea0a35e515fa94201
Elena Yevsyukova <elena.yevs@gmail.com>
2021-01-21 15:10:18 +0200

    Javadoc for `card`

--- src/main/java/com/example/Customer.java
+++ src/main/java/com/example/Customer.java
@@ -6,6 +6,13 @@ package com.example;
  * <p>The customer can have a loyalty card that has a specific number.
  */
 public final class Customer {
+    /**
+     *The customer's loyalty card provides him with bonuses that can be used to pay for purchases.
+     *
+     * <p>The card number must be entered to receive the bonuses.
+     *
+     * <p>The absence of a card does not prevent the customer from purchasing the goods.
+     */
     private final LoyaltyCard card;
 
     public Customer(LoyaltyCard card) {
