fdfb16e14c9e3aa927f4ecf827640def2a75fa66
Elena Yevsyukova <elena.yevs@gmail.com>
2021-04-06 23:19:28 +0200

    add Javadoc for `ShoppingBasket`

--- src/main/java/com/example/ShoppingBasket.java
+++ src/main/java/com/example/ShoppingBasket.java
@@ -1,4 +1,13 @@
 package com.example;
 
+/**
+ * A shopping basket is a thing in which customer puts goods in order to buy it.
+ *
+ * <p>The customer can choose a basket suitable for his purchases.
+ *
+ * <p>The absence of a shopping basket does not prevent the customer from purchasing the goods.
+ *
+ * <p>The customer can change his basket or take another one.
+ */
 abstract class ShoppingBasket implements ShoppingCart{
 }
