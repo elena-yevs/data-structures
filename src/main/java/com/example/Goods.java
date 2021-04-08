11181f2305822ad525d0ad6148550b09fd0ecd29
Elena Yevsyukova <elena.yevs@gmail.com>
2021-04-08 09:33:20 +0200

    edit Javadoc

--- src/main/java/com/example/Goods.java
+++ src/main/java/com/example/Goods.java
@@ -2,8 +2,11 @@ package com.example;
 
 /**
  * A goods item is what the customer comes to the shop for.
- *
- * <p>The selected item is put into the shopping basket or shopping trolley.
  */
 public class Goods {
+
+    /**
+     * The selected item is put into the shopping basket or shopping trolley.
+     */
+    String item;
 }
