5c14ba7ab3e70d64d8167e25623f8415c8724394
Elena Yevsyukova <elena.yevs@gmail.com>
2021-04-08 09:31:09 +0200

    add class `Goods`

---
+++ src/main/java/com/example/Goods.java
@@ -0,0 +1,9 @@
+package com.example;
+
+/**
+ * A goods item is what the customer comes to the shop for.
+ *
+ * <p>The selected item is put into the shopping basket or shopping trolley.
+ */
+public class Goods {
+}
