f6bbb4638eeb5ca4987a62c864e15a37d684c2b7
Elena Yevsyukova <elena.yevs@gmail.com>
2021-04-14 15:37:01 +0200

    Edit Javadoc in `ShoppingCart`

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -1,46 +1,46 @@
 package com.example;
 
 /**
- * A shopping basket is a thing in which customer puts goods in order to buy it.
+ * A shopping cart is a thing in which customer puts goods in order to buy it.
  *
- * <p>The customer can choose a basket for his purchases.
+ * <p>The customer can choose a cart for his purchases.
  *
- * <p>The absence of a shopping basket does not prevent the customer from purchasing the goods.
+ * <p>The absence of a shopping cart does not prevent the customer from purchasing the goods.
  *
- * <p>The customer can change his basket or take another one.
+ * <p>The customer can change his cart or take another one.
  */
 abstract class ShoppingCart {
 
     /**
-     * The shopping basket holds up to 15 items with a total weight of up to 5 kg.
+     * The shopping cart holds up to 15 items with a total weight of up to 5 kg.
      */
     Goods[] itemsInBasket = new Goods[15];
 
     /**
-     * Before purchasing customer should choose a shopping basket.
+     * Before purchasing customer should choose a shopping cart.
      */
     public void chooseShoppingBasket() {
     }
 
     /**
-     * After choosing the basket it have to be taken.
+     * After choosing the cart it have to be taken.
      */
     public void takeShoppingBasket() {
     }
 
     /**
-     * The customer put his goods in the basket.
+     * The customer put his goods in the cart.
      */
     public void putTheGoodsIn() {
     }
     /**
-     * The goods can be put out of the shopping basket.
+     * The goods can be put out of the shopping cart.
      */
     public void putOutTheGoods() {
     }
 
     /**
-     * The basket can be changed to another.
+     * The cart can be changed to another.
      */
     public void changeShoppingBasket() {
     }
